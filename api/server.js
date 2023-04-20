da012f9d8659d3728a6545e0880655924bb07419
Leonardo Firmo <101224821+LeonardoFirmo@users.noreply.github.com>
2023-04-20 17:05:21 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,13 +1,9 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-const cors = require('cors');
 
 server.use(middlewares)
-
-server.use(cors());
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
